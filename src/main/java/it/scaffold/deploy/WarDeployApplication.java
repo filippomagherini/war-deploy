910507d35e898ba050c3b4bc93d321f116963ddf
magherini <maghemagherini@gmail.com>
2018-03-13 14:17:05 +0100

    Initial commit

---
+++ src/main/java/it/scaffold/deploy/WarDeployApplication.java
@@ -0,0 +1,12 @@
+package it.scaffold.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarDeployApplication.class, args);
+	}
+}
